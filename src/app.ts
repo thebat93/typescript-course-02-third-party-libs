6e0525b704e063cbc762b65ce48a2c5a9c0d0c58
thebat93 <ais_93@mail.ru>
2020-07-18 00:57:26 +0300

    Подключение Google Maps

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+const mapElement = document.getElementById('map')!;
 
 type GoogleGeocodingResponse = {
     results: { geometry: { location: { lat: number, lng: number } } }[];
@@ -9,6 +10,11 @@ type GoogleGeocodingResponse = {
 }
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+
+const googleMapsScript = document.createElement("script");
+googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}`;
+googleMapsScript.type = "text/javascript";
+document.body.appendChild(googleMapsScript);
 
 async function searchAddressHandler(event: Event) {
     event.preventDefault();
@@ -22,6 +28,13 @@ async function searchAddressHandler(event: Event) {
         }
 
         const coords = response.data.results[0].geometry.location;
+
+        const map = new google.maps.Map(mapElement, {
+            center: coords,
+            zoom: 8
+        });
+
+        new google.maps.Marker({ position: coords, map });
     } catch (error) {
         console.log(error);
     }
